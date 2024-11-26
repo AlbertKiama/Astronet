7e64860c34632540a663ddaf02e9057b08a278da
Albert Kiama <albertkiamakariuki@gmail.com>
2024-11-26 17:42:09 +0300

    "Updated CSS styles, removed JavaScript code for testimonials, and modified HTML content in index.html"

--- JS/script.js
+++ JS/script.js
@@ -13,41 +13,6 @@ window.onscroll = function() {myFunction()};
       }
     }
 
-/*===========Functionality for Testimonials==========*/       
-
-/*
-var dot = document.getElementsByClassName("dot");
-var slide = document.getElementById("slide");
-
-        dot[0].onclick = function(){
-            slide.style.transform = "translateX(0px)";
-            for(i=0;i<4;i++){
-                dot[i].classList.remove("active");
-            }
-            this.classList.add("active");
-        }
-        dot[1].onclick = function(){
-            slide.style.transform = "translateX(-900px)";
-            for(i=0;i<4;i++){
-                dot[i].classList.remove("active");
-            }
-            this.classList.add("active");
-        }
-        dot[2].onclick = function(){
-            slide.style.transform = "translateX(-1800px)";
-            for(i=0;i<4;i++){
-                dot[i].classList.remove("active");
-            }
-            this.classList.add("active");
-        }
-        dot[3].onclick = function(){
-            slide.style.transform = "translateX(-2700px)";
-            for(i=0;i<4;i++){
-                dot[i].classList.remove("active");
-            }
-            this.classList.add("active");
-        }
-
 /*==================Functionality for my side menu==================*/
 
 
